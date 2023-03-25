4743bfd7cc56407702c2bc7069fb2cd90c1072b4
Lawrence <lawrencebaron027@gmail.com>
2023-03-25 17:25:30 -0600

    Change routes interpolation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const AuthController = require("../controller/authController");
+const { loginUser, registerUser } = require("../controller/authController");
 const router = express.Router();
 
 // @route   GET api/auth/login
 // @desc    Login user
 // @access  Public
-router.post("/login", AuthController.loginUser);
+router.post("/login", loginUser);
 
 // @route   POST api/auth/register
 // @desc    Register user
 // @access  Public
-router.post("/register", AuthController.registerUser);
+router.post("/register", registerUser);
 
 module.exports = router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,71 +1,85 @@
 const express = require("express");
 const router = express.Router();
-const PostController = require("../controller/postController");
+const {
+  getPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+  unlikePost,
+  commentPost,
+  deleteComment,
+  likeComment,
+  unlikeComment,
+  getTimelinePosts,
+  getProfilePosts,
+} = require("../controller/postController");
 const validateToken = require("../utilities/validateToken");
 
 // @route   GET api/posts
 // @desc    Get all posts
 // @access  Public
-router.get("/", PostController.getPosts);
+router.get("/", getPosts);
 
 // @route   GET api/posts/:id
 // @desc    Get post by ID
 // @access  Public
-router.get("/:id", PostController.getPostById);
+router.get("/:id", getPostById);
 
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
-router.post("/", validateToken, PostController.createPost);
+router.post("/", validateToken, createPost);
 
 // @route   PUT api/posts/:id
 // @desc    Update post
 // @access  Private
-router.put("/:id", PostController.updatePost);
+router.put("/:id", updatePost);
 
 // @route   DELETE api/posts/:id
 // @desc    Delete post
 // @access  Private
-router.delete("/:id", PostController.deletePost);
+router.delete("/:id", deletePost);
 
 // @route   PUT api/posts/:id/like
 // @desc    Like a post
 // @access  Private
-router.put("/:id/like", PostController.likePost);
+router.put("/:id/like", likePost);
 
 // @route   PUT api/posts/:id/unlike
 // @desc    Unlike a post
 // @access  Private
-router.put("/:id/unlike", PostController.unlikePost);
+router.put("/:id/unlike", unlikePost);
 
 // @route   POST api/posts/:id/comment
 // @desc    Comment on a post
 // @access  Private
-router.post("/:id/comment", PostController.commentPost);
+router.post("/:id/comment", commentPost);
 
 // @route   DELETE api/posts/:id/comment/:comment_id
 // @desc    Delete comment
 // @access  Private
-router.delete("/:id/comment/:comment_id", PostController.deleteComment);
+router.delete("/:id/comment/:comment_id", deleteComment);
 
 // @route   PUT api/posts/:id/comment/:comment_id/like
 // @desc    Like a comment
 // @access  Private
-router.put("/:id/comment/:comment_id/like", PostController.likeComment);
+router.put("/:id/comment/:comment_id/like", likeComment);
 
 // @route   PUT api/posts/:id/comment/:comment_id/unlike
 // @desc    Unlike a comment
 // @access  Private
-router.put("/:id/comment/:comment_id/unlike", PostController.unlikeComment);
+router.put("/:id/comment/:comment_id/unlike", unlikeComment);
 
 // @route   GET api/posts/timeline/all
 // @desc    Get all posts from following users
 // @access  Private
-router.get("/timeline/all", PostController.getTimelinePosts);
+router.get("/timeline/all", getTimelinePosts);
 
 // @route   GET api/posts/profile/:username
 // @desc    Get all posts from a user
 // @access  Public
-router.get("/profile/:username", PostController.getProfilePosts);
+router.get("/profile/:username", getProfilePosts);
 
 module.exports = router;
