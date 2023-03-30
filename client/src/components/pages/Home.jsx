316b7f9e35b1ff079161abff34303e4281a7eb91
Lawrence <lawrencebaron027@gmail.com>
2023-03-30 13:06:01 -0600

    Added navigate if on /home

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/login");
+  }, [navigate]);
+
   return (
     <>
       <h1 className="bg-blue-500">Home</h1>
