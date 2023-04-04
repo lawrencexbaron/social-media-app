dcc94c12a8dfb18c2ef0e3167723f19aa69c2288
Lawrence <lawrencebaron027@gmail.com>
2023-04-04 09:52:57 -0600

    Added feed to routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Login from "./components/pages/auth/Login";
 import Register from "./components/pages/auth/Register";
+import Feed from "./components/pages/Feed";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="h-full w-full bg-gray-100 min-h-screen flex content-center py-10">
+    <div className="h-full w-full bg-gray-100 min-h-screen flex content-center">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/feed" element={<Feed />} />
         </Routes>
       </Router>
     </div>
