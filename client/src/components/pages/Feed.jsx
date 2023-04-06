d3c050a856b983dc63dda89556f24bdc8e300179
Lawrence <lawrencebaron027@gmail.com>
2023-04-05 20:19:05 -0600

    Main feed post

--- client/src/components/pages/Feed.jsx
+++ client/src/components/pages/Feed.jsx
@@ -16,67 +16,7 @@ function Feed() {
           <div className="sm:w-3/5 h-full sm:sticky mt-16 top-16">
             <div className="bg-white rounded px-6 py-8 border border-slate-200 overflow-y-auto">
               <h1 className="text-2xl font-bold mb-4">Feed</h1>
-              <p className="text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Cupiditate quis, suscipit atque maiores, veniam nihil libero non
-                quidem esse debitis maxime neque rerum omnis ut voluptatibus
-                ipsam. Similique, odit pariatur? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Minima veniam pariatur libero
-                fugiat? Eveniet voluptatem et, iusto recusandae quibusdam
-                accusamus fugiat ipsum, voluptatum inventore repellat minima id
-                magnam illum quas! Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Cupiditate quis, suscipit atque maiores,
-                veniam nihil libero non quidem esse debitis maxime neque rerum
-                omnis ut voluptatibus ipsam. Similique, odit pariatur? Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Minima veniam
-                pariatur libero fugiat? Eveniet voluptatem et, iusto recusandae
-                quibusdam accusamus fugiat ipsum, voluptatum inventore repellat
-                minima id magnam illum quas! Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Cupiditate quis, suscipit atque
-                maiores, veniam nihil libero non quidem esse debitis maxime
-                neque rerum omnis ut voluptatibus ipsam. Similique, odit
-                pariatur? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Minima veniam pariatur libero fugiat? Eveniet voluptatem
-                et, iusto recusandae quibusdam accusamus fugiat ipsum,
-                voluptatum inventore repellat minima id magnam illum quas! Lorem
-                ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-                quis, suscipit atque maiores, veniam nihil libero non quidem
-                esse debitis maxime neque rerum omnis ut voluptatibus ipsam.
-                Similique, odit pariatur? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Minima veniam pariatur libero fugiat? Eveniet
-                voluptatem et, iusto recusandae quibusdam accusamus fugiat
-                ipsum, voluptatum inventore repellat minima id magnam illum
-                quas! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Cupiditate quis, suscipit atque maiores, veniam nihil libero non
-                quidem esse debitis maxime neque rerum omnis ut voluptatibus
-                ipsam. Similique, odit pariatur? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Minima veniam pariatur libero
-                fugiat? Eveniet voluptatem et, iusto recusandae quibusdam
-                accusamus fugiat ipsum, voluptatum inventore repellat minima id
-                magnam illum quas! Lorem ipsum dolor sit amet, consectetur
-                adipisicing elit. Cupiditate quis, suscipit atque maiores,
-                veniam nihil libero non quidem esse debitis maxime neque rerum
-                omnis ut voluptatibus ipsam. Similique, odit pariatur? Lorem
-                ipsum dolor sit amet consectetur adipisicing elit. Minima veniam
-                pariatur libero fugiat? Eveniet voluptatem et, iusto recusandae
-                quibusdam accusamus fugiat ipsum, voluptatum inventore repellat
-                minima id magnam illum quas! Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit. Cupiditate quis, suscipit atque
-                maiores, veniam nihil libero non quidem esse debitis maxime
-                neque rerum omnis ut voluptatibus ipsam. Similique, odit
-                pariatur? Lorem ipsum dolor sit amet consectetur adipisicing
-                elit. Minima veniam pariatur libero fugiat? Eveniet voluptatem
-                et, iusto recusandae quibusdam accusamus fugiat ipsum,
-                voluptatum inventore repellat minima id magnam illum quas! Lorem
-                ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-                quis, suscipit atque maiores, veniam nihil libero non quidem
-                esse debitis maxime neque rerum omnis ut voluptatibus ipsam.
-                Similique, odit pariatur? Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Minima veniam pariatur libero fugiat? Eveniet
-                voluptatem et, iusto recusandae quibusdam accusamus fugiat
-                ipsum, voluptatum inventore repellat minima id magnam illum
-                quas!
-              </p>
+              <p className="text-gray-700">Feed Post </p>
             </div>
           </div>
           <div className="sm:w-1/5 h-full sm:sticky sm:top-16 top-auto block">
