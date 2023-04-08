767108bb8fa243fce81809eacdffceb97de098ea
Lawrence <lawrencebaron027@gmail.com>
2023-04-07 23:47:21 -0600

    New base layout width

--- client/src/components/layout/Base.jsx
+++ client/src/components/layout/Base.jsx
@@ -92,7 +92,7 @@ function Base(props) {
           </div>
         </div>
 
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="max-w-full mx-auto py-6 sm:px-6 lg:px-16">
           {props.children}
         </div>
       </div>
