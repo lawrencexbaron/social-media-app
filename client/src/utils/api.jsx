71727b48461b663aef24c3b5d828f87540e3e098
Lawrence <lawrencebaron027@gmail.com>
2023-04-09 15:37:24 -0600

    exported axios api

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:4000";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: BASE_URL,
   // cors
   headers: {
