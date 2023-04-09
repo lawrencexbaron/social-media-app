a05ff9ae16d05870687df08a7fad82e144108f51
Lawrence <lawrencebaron027@gmail.com>
2023-04-08 20:20:54 -0600

    Updated dynamic feedpost

--- client/src/components/common/FeedPost.jsx
+++ client/src/components/common/FeedPost.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "./Avatar";
 import TextInput from "./TextInput";
 import Button from "./Button";
 import { BiLike, BiComment, BiShare } from "react-icons/bi";
 import NewsFeed from "./NewsFeed";
+import { useMutation, useQuery } from "react-query";
+import { getPosts, createPost } from "../../utils/api";
+import { postStore } from "../../stores/postStore";
 
-function FeedPost({ avatar, onPost }) {
-  const sampleData = [
-    {
-      id: 1,
-      avatar: "https://i.pravatar.cc/150?img=1",
-      author: "John Doe",
-      date: "April 07, 2023",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+function FeedPost({ avatar }) {
+  const [content, setContent] = useState("");
+  const setPosts = postStore((state) => state.setPosts);
+  const {
+    data: posts,
+    isLoading,
+    isError,
+  } = useQuery("posts", () => getPosts());
+
+  const createPostMutation = useMutation(createPost, {
+    onSuccess: (data) => {
+      setPosts((posts) => [...posts, data]);
+      createPostCacheUpdate(data);
     },
-    {
-      id: 2,
-      avatar: "https://i.pravatar.cc/150?img=1",
-      author: "Jane Smith",
-      date: "April 06, 2023",
-      content:
-        "Sed condimentum, nisl ut ultricies lacinia, nisl nisl aliquet nisl...",
-    },
-    // Add more sample data as needed
-  ];
+  });
+
+  const createPostCacheUpdate = (data) => {
+    const updatedPosts = (posts || []).concat(data);
+    createPostMutation.mutate(updatedPosts, { exact: true });
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error...</div>;
+
+  const handlePost = () => {
+    createPostMutation.mutate({ content });
+    setContent("");
+  };
 
   return (
     <div className="sm:w-3/5 h-full sm:sticky mt-16 top-16">
@@ -33,21 +45,24 @@ function FeedPost({ avatar, onPost }) {
           type="text"
           placeholder="What's on your mind?"
           className="w-full border border-gray-200 rounded px-4 py-3"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
         <Button
           type="button"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-          onClick={onPost}
+          onClick={handlePost}
         >
           Post
         </Button>
       </div>
-      {sampleData.map((post) => (
+      {posts.map((post, index) => (
         <NewsFeed
-          key={post.id}
-          avatar={post.avatar}
+          key={index}
+          avatar={post.user}
+          user={post.user}
           author={post.author}
-          date={post.date}
+          date={post.createdAt}
           content={post.content}
         />
       ))}
