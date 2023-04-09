f55bd95294127c089fd4e608c728dd38343e4906
Lawrence <lawrencebaron027@gmail.com>
2023-04-08 20:20:42 -0600

    Added news feed interval from posts

--- client/src/components/common/NewsFeed.jsx
+++ client/src/components/common/NewsFeed.jsx
@@ -3,15 +3,53 @@ import Avatar from "./Avatar"; // Import the Avatar component
 import Button from "./Button"; // Import the Button component
 import { BiLike, BiComment, BiShare } from "react-icons/bi"; // Import the necessary icons from react-icons
 
-const NewsFeed = ({ avatar, author, date, content }) => {
+const NewsFeed = ({ avatar, user, author, date, content }) => {
+  // check if user is object
+  if (typeof user === "object" && user !== null) {
+    // if it is, destructure the user object
+    const { profilePicture, firstname, lastname } = user;
+    // set the avatar to the profilePicture
+    avatar = profilePicture;
+    // set the author to the firstname and lastname
+    author = `${firstname} ${lastname}`;
+  }
+  // add default if avatar is undefined
+  avatar = avatar || "https://via.placeholder.com/150";
+
+  // Format date with posted ago time
+  const postedAgo = (date) => {
+    const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+      return `${interval} years ago`;
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return `${interval} months ago`;
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return `${interval} days ago`;
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return `${interval} hours ago`;
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return `${interval} minutes ago`;
+    }
+    return `${Math.floor(seconds)} seconds ago`;
+  };
+
   return (
     <div className="mt-2 bg-white rounded border-gray-200 border px-6 py-6 flex">
       <div className="flex flex-col w-full justify-start">
         <div className="flex justify-start">
           <Avatar avatar={avatar} size={10} />
           <div className="ml-2">
-            <h1 className="text-lg font-bold">{author}</h1>
-            <p className="text-xs text-slate-500">{date}</p>
+            <h1 className="text-lg">{author}</h1>
+            <p className="text-xs text-slate-500">{postedAgo(date)}</p>
           </div>
         </div>
         <div className="mt-2 border border-t-gray-300 w-full"></div>
