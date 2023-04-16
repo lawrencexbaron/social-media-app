5ae36b72f466fa09aff9bb548ef10ce68b292b58
Lawrence <lawrencebaron027@gmail.com>
2023-04-15 21:52:04 -0600

    If profile is true then dont show post input

--- client/src/components/common/FeedPost.jsx
+++ client/src/components/common/FeedPost.jsx
@@ -5,7 +5,7 @@ import Button from "../common/Button";
 import NewsFeed from "../common/NewsFeed";
 import { usePostStore } from "../../stores/postStore";
 
-function FeedPost({ avatar, posts }) {
+function FeedPost({ avatar, posts, profile }) {
   const [content, setContent] = useState("");
   const { createPost, getPosts } = usePostStore();
 
@@ -23,23 +23,25 @@ function FeedPost({ avatar, posts }) {
 
   return (
     <div className="sm:w-3/5 h-full sm:sticky mt-16 top-16">
-      <div className="bg-white mt-2 rounded px-6 py-5 border border-slate-200 overflow-y-auto flex space-x-2">
-        <Avatar avatar={avatar} size={10} className={`my-auto`} />
-        <TextInput
-          type="text"
-          placeholder="What's on your mind?"
-          className="border-slate-200"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        <Button
-          type="button"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-          onClick={handlePostCreate}
-        >
-          Post
-        </Button>
-      </div>
+      {!profile && (
+        <div className="bg-white mt-2 rounded px-6 py-5 border border-slate-200 overflow-y-auto flex space-x-2">
+          <Avatar avatar={avatar} size={10} className={`my-auto`} />
+          <TextInput
+            type="text"
+            placeholder="What's on your mind?"
+            className="border-slate-200"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <Button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+            onClick={handlePostCreate}
+          >
+            Post
+          </Button>
+        </div>
+      )}
       {posts && posts.length > 0 ? (
         posts.map((post) => {
           return (
