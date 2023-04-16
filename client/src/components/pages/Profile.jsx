38695f0821b847e6ae9ad76207f6bc0d84b624b4
Lawrence <lawrencebaron027@gmail.com>
2023-04-15 21:45:04 -0600

    Fixed alignment by adding blank div

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -23,20 +23,21 @@ function Profile() {
   return (
     <>
       <Base>
-        <div className="sm:flex justify-start space-y-2 sm:space-y-0 sm:space-x-2 mt-2">
+        <div className="sm:flex justify-start space-y-2 sm:space-y-0 sm:space-x-2">
           <ProfileCard
             user={profile}
             name={`${profile.firstname} ${profile.lastname}`}
             avatar={profile.profilePicture}
             coverPhoto={profile.coverPicture}
-            // following={profile.following.length || 0}
-            // followers={profile.followers.length || 0}
+            following={profile.following.length || 0}
+            followers={profile.followers.length || 0}
           />
           <FeedPost
             username={username}
             posts={posts}
             avatar={profile.profilePicture}
           />
+          <div className="sm:w-1/5 h-full sm:sticky sm:top-16 top-auto hidden md:block"></div>
         </div>
       </Base>
     </>
