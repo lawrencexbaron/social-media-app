567cd01b303cb071a491d7ce4bd295a27fe851ea
Lawrence <lawrencebaron027@gmail.com>
2023-07-26 21:47:16 -0600

    Revamped TextInput design

--- client/src/components/common/TextInput.jsx
+++ client/src/components/common/TextInput.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function TextInput(props) {
   return (
-    <div className="relative w-full">
+    <div className='relative w-full'>
       {props.icon && (
-        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+        <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
           {props.icon}
         </span>
       )}
       <input
-        className={`pr-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+        className={`pr-3 appearance-none border rounded w-full py-2 px-3 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-slate-800 ${
           props.className
         } ${props.icon ? "pl-10" : ""}`}
         id={props.id}
