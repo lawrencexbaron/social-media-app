7362e5d730880db98c6bbd44a807f141255d94c1
Lawrence <lawrencebaron027@gmail.com>
2023-07-26 21:47:49 -0600

    Revamped Login design, removed blue accent then added loading state

--- client/src/components/pages/auth/Login.jsx
+++ client/src/components/pages/auth/Login.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useStore } from "zustand";
 import { useAuthStore } from "../../../stores/authStore";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function Login() {
   const navigate = useNavigate();
@@ -20,9 +21,15 @@ function Login() {
     try {
       await login(email, password);
       setTimeout(() => {
-        navigate("/feed");
+        // get token from localstorage
+        const token = localStorage.getItem("token");
+        // if token is not empty
+        if (token) {
+          // navigate to feed page
+          navigate("/feed");
+        }
         setSuccess(false);
-      }, 1000);
+      }, 1500);
     } catch (error) {
       console.log(error);
     }
@@ -30,14 +37,16 @@ function Login() {
 
   return (
     <>
-      <div className="mx-auto bg-white shadow-md px-6 py-8 w-3/4 md:w-1/2 lg:w-1/4 h-3/4 my-10">
-        <h1 className="text-2xl font-bold text-center">Login</h1>
+      <div className='mx-auto bg-white shadow-md px-6 py-8 w-3/4 md:w-1/2 lg:w-1/4 h-3/4 my-10'>
+        <h1 className='text-2xl font-bold text-center'>Login</h1>
+        <p className='text-gray-600 text-sm text-center'>
+          Login to your account to continue
+        </p>
         {error && (
           <div
-            className="bg-red-100 border flex flex-col border-red-400 text-red-700 px-4 py-3 rounded relative mt-4"
-            role="alert"
+            className='flex flex-col text-red-700 relative mt-2'
+            role='alert'
           >
-            <p className="font-bold">Error!</p>
             {Array.isArray(error.message) ? (
               <ul>
                 {error.message.map((msg, index) => (
@@ -49,40 +58,39 @@ function Login() {
             )}
           </div>
         )}
-        {success && (
-          <div
-            className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mt-4"
-            role="alert"
-          >
-            <p className="font-bold">Success!</p>
-            <p className="block sm:inline">You are logged in!</p>
-            <p className="block">Redirecting...</p>
-          </div>
-        )}
-
-        <form className="mt-4" onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <Label htmlFor="email">Email</Label>
+        <form className='mt-4' onSubmit={handleSubmit}>
+          <div className=''>
+            <Label htmlFor='email'>Email</Label>
             <TextInput
-              id="email"
-              type="email"
-              placeholder="Email"
+              id='email'
+              type='email'
+              placeholder='Email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="mb-2"
+              className='mb-2'
             />
 
-            <Label htmlFor="password">Password</Label>
+            <Label htmlFor='password'>Password</Label>
             <TextInput
-              id="password"
-              type="password"
-              placeholder="Password"
+              id='password'
+              type='password'
+              placeholder='Password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="mb-2"
+              className='mb-2'
             />
           </div>
-          <div className="flex  items-center justify-between space-x-2">
+          <Button
+            type='submit'
+            className={`bg-slate-800 hover:bg-slate-700 font-semibold text-sm text-white w-full my-2`}
+          >
+            {success ? (
+              <AiOutlineLoading3Quarters className='animate-spin mx-auto text-xl' />
+            ) : (
+              "Login"
+            )}
+          </Button>
+          <div className='flex  items-center justify-between space-x-2'>
             {/* <a
               className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
               href="#"
@@ -90,26 +98,19 @@ function Login() {
               Register now
             </a> */}
             <Link
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              to="/register"
+              className='inline-block align-baseline font-semibold text-sm text-slate-800 hover:text-slate-600'
+              to='/register'
             >
               Register now
             </Link>
             <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
+              className='inline-block align-baseline font-semibold text-sm text-slate-800 hover:text-slate-600'
+              href='#'
             >
               Forgot Password?
             </a>
           </div>
-          <div className="flex items-center justify-end mt-2">
-            <Button
-              type="submit"
-              className={`bg-blue-500 hover:bg-blue-400 text-white`}
-            >
-              {isLoading ? "Loading..." : "Login"}
-            </Button>
-          </div>
+          <div className='flex items-center justify-end mt-2'></div>
         </form>
       </div>
     </>
