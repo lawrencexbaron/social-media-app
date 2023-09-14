197479094e48b6a3e9aa62976495b0efa4c73500
Lawrence <lawrencebaron027@gmail.com>
2023-09-13 22:11:53 -0600

    Added New font

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,13 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <link
+      rel='preload'
+      href='Mona-Sans.woff2'
+      as='font'
+      type='font/woff2'
+      crossorigin
+    ></link>
     <QueryClientProvider client={new QueryClient()}>
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
