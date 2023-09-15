8bfc30310b0ef1919f982884e1d784c3e3bd5021
Lawrence <lawrencebaron027@gmail.com>
2023-09-15 11:02:10 -0600

    Re-added followers and following modal on click

--- client/src/components/common/ProfileCard.jsx
+++ client/src/components/common/ProfileCard.jsx
@@ -124,30 +124,21 @@ function ProfileCard({
                 <p className='font-bold text-base'>0</p>
                 <p className='text-sm text-gray-600 font-semibold'>Posts</p>
               </div>
-              <div className='flex flex-col'>
-                <p className='font-bold text-base'>0</p>
+              <div
+                className='flex flex-col hover:cursor-pointer'
+                onClick={openFollowersModal}
+              >
+                <p className='font-bold text-base'>{followers}</p>
                 <p className='text-sm text-gray-600 font-semibold'>Followers</p>
               </div>
-              <div className='flex flex-col'>
-                <p className='font-bold text-base'>0</p>
+              <div
+                className='flex flex-col hover:cursor-pointer'
+                onClick={openFollowingModal}
+              >
+                <p className='font-bold text-base'>{following}</p>
                 <p className='text-sm text-gray-600 font-semibold'>Following</p>
               </div>
             </div>
-            {/* <div className='inline-flex mx-auto items-center px-2 border border-gray-200 rounded-full'>
-              <div
-                className='px-2 py-2 text-xs md:text-xs cursor-pointer'
-                onClick={openFollowersModal}
-              >
-                <span className='font-bold'>{followers}</span> Followers
-              </div>
-              <div className='w-px h-full mx-2 bg-gray-200'></div>
-              <div
-                className='px-2 py-2 text-xs md:text-xs cursor-pointer'
-                onClick={openFollowingModal}
-              >
-                <span className='font-bold'>{following}</span> Following
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
