f62d1cedf89b640bdab886777471bce8d8da0a0d
Lawrence <lawrencebaron027@gmail.com>
2023-10-05 15:45:58 -0600

    Removed user cnosole

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -70,7 +70,6 @@ const getUserById = async (req, res) => {
 
     // get user then include posts and populate following and followers
     const user = await User.findById(req.params.id).populate("posts");
-    console.log(user);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
